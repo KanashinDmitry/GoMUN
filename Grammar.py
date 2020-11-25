76f7b9d9e08f590f6b8ff94c1ac321322c7e1e83
Lunev Artem <artem.e.lunev2000@gmail.com>
2020-11-25 13:47:44 +0300

    multiple productions can be used

--- Grammar.py
+++ Grammar.py
@@ -10,30 +10,16 @@ class Grammar:
 
     def belongs(self, word: str):
         def list_contains_another(small, big):
+            res = []
             if len(big) < len(small):
-                return False
+                return []
             for i in range(len(big) - len(small) + 1):
                 for j in range(len(small)):
                     if big[i + j] != small[j]:
                         break
                 else:
-                    return i, i + len(small)
-            return False
-
-        def dup_list(lst):
-            result = []
-
-            for item in lst:
-                result.append(item)
-
-            return result
-
-        def convert_list_to_word(lst):
-            converted_word = ""
-            for l in lst:
-                converted_word += l
-
-            return converted_word
+                    res.append((i, i + len(small)))
+            return res
 
         queue = Queue()
 
@@ -45,26 +31,28 @@ class Grammar:
             #print(f"Got {tmp, prods} from {queue}")
             for x in self.productions:
                 list1, list2 = x
+
                 indexes = list_contains_another(list1, tmp)
-                if not indexes:
+                if len(indexes) == 0:
                     continue
 
-                ind_start, ind_end = indexes
+                for ind in indexes:
+                    ind_start, ind_end = ind
 
-                res = dup_list(tmp)
+                    res = tmp.copy()
 
-                res_part1 = [res[i] for i in range(ind_start)]
-                res_part2 = list2
-                res_part3 = [res[i] for i in range(ind_end, len(res))]
+                    res_part1 = [res[i] for i in range(ind_start)]
+                    res_part2 = list2
+                    res_part3 = [res[i] for i in range(ind_end, len(res))]
 
-                final_list = [r for r in res_part1 + res_part2 + res_part3 if r != 'eps']
-                print(f'{" ".join(tmp)} -> {final_list}')
-                prods_dup = prods.copy()
-                prods_dup.append(x)
+                    final_list = [r for r in res_part1 + res_part2 + res_part3 if r != 'eps']
+                    print(f'{" ".join(tmp)} -> {final_list}')
+                    prods_dup = prods.copy()
+                    prods_dup.append(x)
 
-                queue.put((final_list, prods_dup))
-                #print(final_list)
-                if convert_list_to_word(final_list) == word:
-                    return prods_dup
+                    queue.put((final_list, prods_dup))
+                    #print(final_list)
+                    if "".join(final_list) == word:
+                        return prods_dup
 
         return False
